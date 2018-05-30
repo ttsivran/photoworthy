01b07383ade5c89ad1381766c3adbd6f5f314a36
Theodore Tsivranidis <theodoros_tsivranidis@brown.edu>
2018-05-30 17:47:29 +0300

    Initial Commit

---
+++ Photoworthy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photoworthy
+//
+//  Created by Theodore Tsivranidis on 5/30/18.
+//  Copyright © 2018 Teo Tsivranidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
